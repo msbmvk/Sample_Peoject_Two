a486d33f11355e40ca4f0c8160365813a865d7aa
Surendra Babu M <Surendra Babu M@LAPTOP-56FV87EB>
2024-11-10 21:27:25 +0530

    Testcalss updated

--- GitgubPush/src/test/java/TestClass.java
+++ GitgubPush/src/test/java/TestClass.java
@@ -4,6 +4,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello Muppraju surendra");
+			System.out.println("Hello Lakshmi Durga");
+			String a = "A SD F G H Jsd sd sds";
+			String[] stringa= a.split(" /n");
+			System.out.println(stringa);
+			System.out.println("Hello Muppraju surendra");
 			
 	}
 
